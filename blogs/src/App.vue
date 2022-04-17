dee67f8baa82a78f7daa945f6bbc232f9936d1f0
Justin Scott Jenecke <93656319+codespace-justin@users.noreply.github.com>
2022-04-17 19:54:31 +0200

    implements slots for FormHelper component

--- blogs/src/App.vue
+++ blogs/src/App.vue
@@ -1,28 +1,76 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main id="app">
+
+    <!-- FormHelper.vue -->
+    <form-helper>
+      <div slot="form-header"> 
+         <h2>
+            Blog Creator
+          </h2>
+         <p>
+            Use the provided form to create new Blogs
+         </p>
+      </div>
+
+      <div slot="form-fields"> 
+        <input type="text" placeholder="name" required>
+        <input type="password" placeholder="password" required>
+      </div>
+
+      <div slot="form-controls"> 
+        <input 
+          type="submit"
+          v-on:click.prevent="handleSubmit"
+          value="Submit"
+        >
+      </div>
+    </form-helper>
+    <!-- FormHelper.vue -->
+
+  </main>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+<script>
+  import FormHelper from './components/FormHelper.vue';
+
+  export default {
+
+    // Component Name
+    name: 'App',
+
+    // Sub-Components 
+    components : {
+      'form-helper' : FormHelper
+    
+    },
+
+    // Data
+    data() {
+      return {
+        title : "This is the Slot Title",
+
+        text : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. \n'
+          + 'Rerum, eos dignissimos! Eligendi nostrum id cupiditate veniam dolor, \n '
+          + 'mollitia hic libero repudiandae harum aliquid quos doloremque \n'
+          + 'quas, sunt nisi quam totam.'
+      }
+    },
+
+    // methods
+    methods : {
+
+    }
+
   }
-}
 </script>
+
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ blogs/src/components/FormHelper.vue
@@ -0,0 +1,66 @@
+<template>
+    <div id="form-helper">
+
+        <h1>Please fill out our form...</h1>
+        <form action="" id="form-helper-form">
+            <div id="form-header">
+                <slot name="form-header"/> <!--slot-->
+            </div>
+            <div id="form-fields"> 
+                <slot name="form-fields"/> <!--slot-->
+            </div>
+            <div id='form-controls'>
+                <slot name="form-controls"/> <!--slot-->
+            </div>
+            <div id="useful-links">
+                <ul>
+                    <li>
+                        <a href="#">link 1</a>
+                    </li>
+                    <li>
+                        <a href="#">link 2</a>
+                    </li>
+                    <li>
+                        <a href="#">link 3</a>
+                    </li>
+                    <li>
+                        <a href="#">link 4</a>
+                    </li>
+                </ul>
+            </div>
+        </form>
+
+    </div>
+</template>
+
+
+<script>
+    
+    export default ({
+        name : 'form-helper',
+
+        data() {
+            return {
+                sampleText : 'This is the Form Helper component'
+            }
+        }
+    })
+
+</script>
+
+
+<style scoped>
+
+    #form-helper-form {
+        display: flex;
+        flex-direction: column;
+        padding: 1rem;
+    }
+
+    #form-fields ul {
+        display: flex;
+        flex-direction: column;
+        padding: 1rem;
+    }
+
+</style>
